618194781a63996c06a40485dfc8ebe44c659da0
bulutluoz <abbulutluoz@gmail.com>
2024-07-16 12:44:38 +0200

    5.gun sonu

--- src/test/java/day05_maven_JUnitFramework/C06_BeforeAll_AfterAll.java
+++ src/test/java/day05_maven_JUnitFramework/C06_BeforeAll_AfterAll.java
@@ -3,10 +3,14 @@ package day05_maven_JUnitFramework;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
+import java.util.List;
 
 public class C06_BeforeAll_AfterAll {
 
@@ -26,6 +30,8 @@ public class C06_BeforeAll_AfterAll {
 
 
     static WebDriver driver;
+    static List<WebElement> bulunanUrunElementleriList;
+
     @BeforeAll
     public static void setup(){
         driver = new ChromeDriver();
@@ -41,14 +47,44 @@ public class C06_BeforeAll_AfterAll {
     @Test
     public void test01(){
         // 1- testotomasyonu anasayfaya gidin ve dogru sayfaya gittiginizi test edin
+        driver.get("https://www.testotomasyonu.com");
+
+        String expectedUrl = "https://www.testotomasyonu.com/";
+        String actualUrl = driver.getCurrentUrl();
+
+        if (expectedUrl.equals(actualUrl)){
+            System.out.println("Anasayfa testi PASSED");
+        }else System.out.println("Anasayfa testi FAILED");
     }
     @Test
     public void test02(){
         // 2- phone icin arama yapin ve arama sonucunda urun bulunabildigini test edin
+
+        WebElement aramaKutusu = driver.findElement(By.id("global-search"));
+        aramaKutusu.sendKeys("phone" + Keys.ENTER);
+
+        bulunanUrunElementleriList = driver.findElements(By.xpath("//*[@class='product-detail mb-3']"));
+
+        if (bulunanUrunElementleriList.size() > 0){
+            System.out.println("Urun arama testi PASSED");
+        }else System.out.println("Urun arama testi FAILED");
+
     }
     @Test
     public void test03(){
         // 3- ilk urunu tiklayin ve urun isminde case sensitive olmadan phone bulundugunu test edin
+
+        bulunanUrunElementleriList.get(0).click();
+
+        String expectedUrunIcerigi = "phone";
+
+        WebElement urunIsimElementi = driver.findElement(By.xpath("//*[@class=' heading-sm mb-4']"));
+        String actualIsim = urunIsimElementi.getText().toLowerCase();
+
+
+        if (actualIsim.contains(expectedUrunIcerigi)){
+            System.out.println("Urun ismi testi PASSED");
+        }else System.out.println("Urun ismi testi FAILED");
     }
 
 
