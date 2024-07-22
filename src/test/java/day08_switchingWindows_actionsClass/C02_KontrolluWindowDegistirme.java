a9e102f3ffa4d35d1de2f096866d906d2ed5ea14
bulutluoz <abbulutluoz@gmail.com>
2024-07-22 10:14:12 +0200

    8.gun sonu

--- src/test/java/day08_switchingWindows_actionsClass/C02_KontrolluWindowDegistirme.java
+++ src/test/java/day08_switchingWindows_actionsClass/C02_KontrolluWindowDegistirme.java
@@ -1,7 +1,9 @@
 package day08_switchingWindows_actionsClass;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WindowType;
 import utilities.ReusableMethods;
 import utilities.TestBaseEach;
@@ -17,6 +19,7 @@ public class C02_KontrolluWindowDegistirme extends TestBaseEach {
 
         System.out.println("Anasayfa : "+ driver.getCurrentUrl()); // https://www.testotomasyonu.com/
         System.out.println("Anasayfa : "+ driver.getWindowHandle()); // B3EAA570CB2910434B41A8E608AF22F2
+        String ilkWindowWhd = driver.getWindowHandle();
         ReusableMethods.bekle(3);
 
         // Yeni bir tab olusturup electronics linkine tiklayin
@@ -49,6 +52,7 @@ public class C02_KontrolluWindowDegistirme extends TestBaseEach {
         // 83E84E3762FDEB38AF2F6DB152CD78AF
         System.out.println("Electronics linkine basinca tum WHD'leri : "+ driver.getWindowHandles());
         // 83E84E3762FDEB38AF2F6DB152CD78AF
+        String ikinciWindowWhd = driver.getWindowHandle();
 
         ReusableMethods.bekle(3);
 
@@ -65,20 +69,43 @@ public class C02_KontrolluWindowDegistirme extends TestBaseEach {
 
         driver.get("https://www.wisequarter.com");
 
+        String ucuncuWindowWhd = driver.getWindowHandle();
+
+        System.out.println("=============================");
+        System.out.println("Birinci window icin WHD : "  + ilkWindowWhd);
+        System.out.println("Ikinci window icin WHD : "  + ikinciWindowWhd);
+        System.out.println("Ucuncu window icin WHD : "  + ucuncuWindowWhd);
+
 
         // testotomasyonu anasayfanin acik oldugu window'a donun ve
         // anasayfada oldugunuzu test edin
+        driver.switchTo().window(ilkWindowWhd);
+        String expectedUrl = "https://www.testotomasyonu.com/";
+        String actualUrl = driver.getCurrentUrl();
 
+        Assertions.assertEquals(expectedUrl,actualUrl);
+        ReusableMethods.bekle(2);
 
         // electronics urunlerin oldugu window'a gidin
         // Home/Electronics yazdigini test edin
 
+        driver.switchTo().window(ikinciWindowWhd);
+        WebElement seciliKategoryElementi = driver.findElement(By.xpath("//li[@class='current']"));
+
+        String expectedSeciliKategori = "Electronics";
+        String actualSeciliKategori = seciliKategoryElementi.getText();
+
+        Assertions.assertEquals(expectedSeciliKategori,actualSeciliKategori);
+        ReusableMethods.bekle(2);
 
         // wisequarter'in acik oldugu window'a gidin
         // url'in wisequarter icerdigini test edin
+        driver.switchTo().window(ucuncuWindowWhd);
 
+        String expectedUrlIcerik = "wisequarter";
+        actualUrl = driver.getCurrentUrl();
 
-
+        Assertions.assertTrue(actualUrl.contains(expectedUrlIcerik));
 
         ReusableMethods.bekle(2);
     }
